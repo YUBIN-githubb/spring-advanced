a3f0e00d6c89ede0d5560d4d54ba5fde2c95d7d3
Moon YuBin <yubin5744@gmail.com>
2025-02-26 11:55:33 +0900

    레벨 1-3 : validation

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,6 +1,7 @@
 package org.example.expert.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,6 +13,10 @@ public class UserChangePasswordRequest {
 
     @NotBlank
     private String oldPassword;
+
+    //requestDto에서 검증할 수 있도록 수정
     @NotBlank
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}",
+            message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
     private String newPassword;
 }
